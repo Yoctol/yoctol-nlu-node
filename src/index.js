58ce8b709ecf8d151e8f5b6d8e410653fd18ce83
kpman <s92f002@hotmail.com>
2018-08-06 12:44:12 +0800

    Add update-notifier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
+const updateNotifier = require('update-notifier');
+
+const pkg = require('../package.json');
+
 const Client = require('./Client');
 const IntentClassifier = require('./IntentClassifier');
 const EntityExtractor = require('./EntityExtractor');
+
+updateNotifier({ pkg }).notify({ defer: false });
 
 exports.Client = Client;
 exports.IntentClassifier = IntentClassifier;
