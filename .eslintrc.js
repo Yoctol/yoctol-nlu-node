a172f1c96caf13f82ac675830b5311a940e95dd1
吳東曄 Wu, Dung-Ie <tw0517tw@gmail.com>
2018-09-29 01:41:28 +0800

    bump deps (#19)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,11 @@
 module.exports = {
-  extends: ['yoctol-base', 'prettier'],
+  extends: ['yoctol-base'],
   env: {
     node: true,
     jest: true,
     jasmine: true,
   },
-  plugins: ['prettier'],
   rules: {
-    'class-methods-use-this': 'off',
-    'consistent-return': 'off',
     'no-param-reassign': 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        trailingComma: 'es5',
-        singleQuote: true,
-      },
-    ],
   },
 };
